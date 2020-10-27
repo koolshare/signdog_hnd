168d48eb8e6d2ecdbac0d34228cc1862e02821e4
sadoneli <sadoneli@gmail.com>
2020-10-27 21:17:06 +0800

    repack

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "签到狗3.0，一签就到", 
     "home_url": "Module_signdog.asp", 
-    "md5": "f3039e7902d04a24e901d82dbc71bf00", 
+    "md5": "36dc3faa442f3ae36abb0e3474f49451", 
     "module": "signdog", 
     "title": "签到狗3.0", 
     "version": "1.2"
