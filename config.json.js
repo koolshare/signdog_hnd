47df9421ae118ff968dbbbe15ce283f59713a682
Carseason <757841203@qq.com>
2022-02-09 04:23:20 +0800

    udpate 1.5.0

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "签到狗3.0，一签就到", 
     "home_url": "Module_signdog.asp", 
-    "md5": "b5f680bd915d69d39600f32912e4380c", 
+    "md5": "15fb0df08362ef83813a32a1e7ab2065", 
     "module": "signdog", 
     "title": "签到狗3.0", 
-    "version": "1.4.10"
+    "version": "1.5.0"
 }
