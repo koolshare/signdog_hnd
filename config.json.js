d4fa546d9e5d10d6cb228144d16f533a4cb53f9b
Carseason <757841203@qq.com>
2022-03-02 09:21:16 +0800

    del gcc cgo-sqlite

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "签到狗3.0，一签就到", 
     "home_url": "Module_signdog.asp", 
-    "md5": "288c44c67104ba675a38f235ec53b7cb", 
+    "md5": "0e318fb577257d960e730e2f23866ca5", 
     "module": "signdog", 
     "title": "签到狗3.0", 
-    "version": "1.5.4"
+    "version": "1.5.5"
 }
