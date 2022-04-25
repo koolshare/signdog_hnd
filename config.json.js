a0d0efb1b96b12552eb3fe4296f54d2c2456bf57
Carseason <757841203@qq.com>
2022-04-25 15:17:35 +0800

    sync 1.2.11

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "签到狗3.0，一签就到", 
     "home_url": "Module_signdog.asp", 
-    "md5": "25482b73906b4eeefd37ac9b418a8bd0", 
+    "md5": "8ba1b87132f23a95a7451787819e8b0a", 
     "module": "signdog", 
     "title": "签到狗3.0", 
-    "version": "1.6.3"
+    "version": "1.6.4"
 }
